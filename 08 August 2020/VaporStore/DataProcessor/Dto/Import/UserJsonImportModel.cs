6ba9663785f83dbd16a94be6db994a466625263a
denadox <108186224+denadox@users.noreply.github.com>
2022-09-05 12:10:03 +0200

    Add files via upload

---
+++ 08 August 2020/VaporStore/DataProcessor/Dto/Import/UserJsonImportModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class UserJsonImportModel
+    {
+        [Required]
+        [RegularExpression("[A-Z][a-z]{2,} [A-Z][a-z]{2,}")]
+        public string FullName { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Range(3, 103)]
+        public int Age { get; set; }
+        public IEnumerable<CardJsonInputModel> Cards { get; set; }
+    }
+}
